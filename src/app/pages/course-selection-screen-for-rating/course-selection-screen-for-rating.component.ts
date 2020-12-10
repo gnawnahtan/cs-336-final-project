c371c4de2e2bb24dc2de26400e772dcec5c3f1fb
Nathan W <gnawnahtan@gmail.com>
2020-12-09 21:46:43 -0500

    FIXED DATA() ERROR

--- src/app/pages/course-selection-screen-for-rating/course-selection-screen-for-rating.component.ts
+++ src/app/pages/course-selection-screen-for-rating/course-selection-screen-for-rating.component.ts
@@ -3,21 +3,12 @@ import { AngularFirestore } from "@angular/fire/firestore";
 import { AngularFireModule } from '@angular/fire';
 import { ActivatedRouteSnapshot } from '@angular/router';
 
-interface Department {
-  name: string;
-  viewName: string
-}
-
-interface Course {
-  name: string;
-  viewName: string
-}
-
 @Component({
   selector: 'app-course-selection-screen-for-rating',
   templateUrl: './course-selection-screen-for-rating.component.html',
   styleUrls: ['./course-selection-screen-for-rating.component.css']
 })
+
 export class CourseSelectionScreenForRatingComponent implements OnInit {
 
   public selectedDepartment: string;
@@ -30,14 +21,14 @@ export class CourseSelectionScreenForRatingComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.database.collection('departments')
+    this.database.collection<Department>('departments')
       .get()
       .subscribe(res => {
         res.docs.forEach((doc) => {
           this.departments.push(doc.data());
         });
       });
-    this.database.collection('courses')
+    this.database.collection<Courses>('courses')
       .get()
       .subscribe(res => {
         res.docs.forEach((doc) => {
@@ -48,18 +39,29 @@ export class CourseSelectionScreenForRatingComponent implements OnInit {
 
 
 
-  csCourses: Course[] = [
-    { name: 'CS 108', viewName: 'CS 108' },
-    { name: 'CS 112', viewName: 'CS 112' },
-    { name: 'CS 212', viewName: 'CS 212' },
-  ];
-  mathCourses: Course[] = [
-    { name: 'Math 251', viewName: 'Math 251' },
-    { name: 'Math 252', viewName: 'Math 252' },
-    { name: 'Stat 243', viewName: 'Stat 243' },
-  ];
-  engrCourses: Course[] = [
-    { name: 'Engr 220', viewName: 'Engr 220' },
-  ];
+  // csCourses: Course[] = [
+  //   { name: 'CS 108', viewName: 'CS 108' },
+  //   { name: 'CS 112', viewName: 'CS 112' },
+  //   { name: 'CS 212', viewName: 'CS 212' },
+  // ];
+  // mathCourses: Course[] = [
+  //   { name: 'Math 251', viewName: 'Math 251' },
+  //   { name: 'Math 252', viewName: 'Math 252' },
+  //   { name: 'Stat 243', viewName: 'Stat 243' },
+  // ];
+  // engrCourses: Course[] = [
+  //   { name: 'Engr 220', viewName: 'Engr 220' },
+  // ];
 
+}
+
+interface Department {
+  title: string;
+}
+
+interface Courses {
+  id: number;
+  title: string;
+  description: string;
+  department: string;
 }
