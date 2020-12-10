a6788e08620a0631311336f2f3d77acdd4d66c7c
Bongo9911 <dawsonbuist@gmail.com>
2020-12-09 22:24:15 -0500

    added selectable chips for question 2 and 3

--- src/app/pages/question-two-for-rating/question-two-for-rating.component.ts
+++ src/app/pages/question-two-for-rating/question-two-for-rating.component.ts
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuestionTwoForRatingComponent implements OnInit {
 
+  commendations : Commendation[] = [{name: 'Low homework', selected: false, color: 'primary'}, {name: 'Easy grader', selected: false, color: '#00ff00'},];
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+}
+
+interface Commendation {
+  name: string;
+  selected: boolean;
+  color: string;
 }
