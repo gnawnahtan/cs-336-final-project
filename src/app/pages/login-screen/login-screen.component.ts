c22db808fbf904b6fc9d5de571757a86d09124c9
jackwestel <orlynao19@gmail.com>
2020-12-13 20:58:45 -0500

    login code :3

--- src/app/pages/login-screen/login-screen.component.ts
+++ src/app/pages/login-screen/login-screen.component.ts
@@ -9,9 +9,15 @@ import * as firebase from 'firebase';
 })
 export class LoginScreenComponent implements OnInit {
 
-  constructor(private firestore: AngularFirestore) { }
+  accounts = [];
+  constructor(private db: AngularFirestore) { }
 
   ngOnInit(): void {
+    this.db.collection<Item>('/users', ref => ref.orderBy('username')).valueChanges({ idField: 'docID' }).subscribe(res => {
+      this.accounts = [];
+      res.forEach(element =>
+        this.accounts.push(element)
+      )});
   }
 
   login() {
@@ -24,6 +30,36 @@ export class LoginScreenComponent implements OnInit {
     // });
   }
 
+  checkCreds(){
+    let userVal;
+    let passVal;
+  
+    let username = document.getElementById('loginU')
+    if(username) 
+    {
+      userVal = (username as HTMLInputElement).value;
+    }
+  
+  
+    let password = document.getElementById('loginP')
+    if(password)
+    {
+      passVal = (password as HTMLInputElement).value;
+    }
+    
+    let exists = false;
+    this.accounts.forEach(function (value){
+      if (value.username == userVal && value.password == passVal) exists = true;
+    })
+    return exists
+  }
+
+
+}
+
+interface Item {
+  username: string,
+  password: string
 }
 
 export interface ProfessorRec {
--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,  } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private db: AngularFirestore) {}
 
   ngOnInit(): void {
+    //this.db.collection<Item>('/users')//, ref => ref.orderBy('timestamp')).valueChanges({ idField: 'docID' }).subscribe(res => {
+    //   this.messages = [];
+    //   res.forEach(element =>
+    //     this.messages.push(element)
+    //   )});
+  }
+
+  async accountGen(){
+    let data = []
+  
+  
+    let username = document.getElementById('loginU')
+    if(username) 
+    {
+      let val = (username as HTMLInputElement).value;
+      data.push({key: "username", value: val})
+    }
+  
+  
+    let password = document.getElementById('loginP')
+    if(password)
+    {
+      let val = (password as HTMLInputElement).value;
+      data.push({key: "password", value: val})
+    }
+    await this.db.collection('users').add(data);
   }
 
 }
+
+// interface Item{
+//   username: string,
+//   password: string
+// }
